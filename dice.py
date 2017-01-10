a00d4fef35b8a9d1cbe2a3f87fcd1abce9916970
stef090 <stjepan.lovasic@gmail.com>
2017-01-10 23:31:12 +0100

    10.01.2017. : Finished interface, coupled interface to game logic.

--- dice.py
+++ dice.py
@@ -1,93 +1,92 @@
-import sys
 from tkinter import *
 import random
 import time
-
-class Dice(object):
-
-    _name = 'Dice'
-
-    def __init__(self):
-        self.number = 0
 
 
 class DiceGame(object):
     _name = 'Dice Game'
 
     def __init__(self):
-        dice1 = Dice()
-        dice2 = Dice()
         self.score = {'player': 0,
                       'computer': 0}
-        self.startGame(dice1, dice2)
 
     def check_input(self, text):
         states = {'N': False,
                   'Y': True}
         return states.get(text)
 
-    def startGame(self, dice1, dice2):
-        run = 1
-        print("Welcome to Dice!")
-        while run:
-            run_prompt = ''
-            while run_prompt not in ('N','Y'):
-                run_prompt = str(input("Would you like to throw dice? (Y\\N)")).upper()
-            if not self.check_input(run_prompt.upper()):
-                print("The score is: Player "
-                      + str(self.score['player'])
-                      + " : "
-                      + str(self.score['computer'])
-                      + " Computer")
-                break
-            dice1.number = random.randrange(0,6)
-            time.sleep(2)
-            print('You threw a ' + str(dice1.number))
-            dice2.number = random.randrange(0,6)
-            time.sleep(2)
-            print('Computer threw a ' + str(dice2.number))
-
-            if dice1.number > dice2.number:
-                self.score['player'] += 1
-                print('You win!')
-            elif dice1.number < dice2.number:
-                self.score['computer'] += 1
-                print('You lose! Too bad!')
-            else:
-                print('It\'s a draw!')
+    def throw_dice(self):
+        print("Dice thrown!")
+        dice_player = random.randrange(0, 6)
+        dice_computer = random.randrange(0, 6)
+        if dice_player > dice_computer:
+            self.score["player"] += 1
+        elif dice_computer > dice_player:
+            self.score["computer"] += 1
+        return dice_player, dice_computer, self.score
 
 
 class Display(object):
 
     def __init__(self, master):
+        game = DiceGame()
+
         def yes_button_click(event):
+            redraw_labels()
             play_label["text"] = 'Very well!'
-            print('Very well!')
+            notice_label["text"] = 'Dice thrown'
+            result = game.throw_dice()
+            player_dice_label["text"] += str(result[0])
+            computer_dice_label["text"] += str(result[1])
+            player_label["text"] += str(result[2]["player"])
+            computer_label["text"] += str(result[2]["computer"])
+
+
         def no_button_click(event):
             play_label["text"] = 'See you later!'
+            time.sleep(3)
             print('See you later!')
+            root.quit()
 
+        def redraw_labels():
+            player_dice_label["text"] = 'Player Dice: '
+            computer_dice_label["text"] = 'Computer Dice: '
+            player_label["text"] = 'Player: '
+            computer_label["text"] = 'Computer: '
+
+        #Frame definition
         frame_upper = Frame(master)
         frame_upper.pack()
-        game_label = Label(frame_upper, text='Welcome to Dice!')
-        game_label.pack()
         frame_lower = Frame(master)
         frame_lower.pack(side=BOTTOM)
+
+        #Upper Frame
+        game_label = Label(frame_upper, text='Welcome to Dice!')
+        game_label.grid(row=0, column=1, sticky=N)
         play_label = Label(frame_upper, text='Would you like to play dice?', foreground='blue')
-        play_label.pack()
-        player_label = Label(frame_lower, text='Player', foreground='blue', pady=20)
-        computer_label = Label(frame_lower, text='Computer', foreground='red', pady=20)
-        player_label.pack(side=LEFT)
-        computer_label.pack(side=RIGHT)
+        play_label.grid(row=1, column=1)
         yes_button = Button(frame_upper, text='Yes', fg='blue', padx=5)
         no_button = Button(frame_upper, text='No', fg='red', padx=5)
         yes_button.bind("<Button-1>", yes_button_click)
         no_button.bind("<Button-1>", no_button_click)
-        yes_button.pack(side=LEFT)
-        no_button.pack(side=RIGHT)
+        yes_button.grid(row=2, column=0, sticky=W)
+        no_button.grid(row=2, column=2, sticky=E)
+        notice_label = Label(frame_upper, text='', foreground='blue')
+        notice_label.grid(row=3, column=1)
+        player_dice_label = Label(frame_upper, text='Player Dice: ', foreground='blue')
+        computer_dice_label = Label(frame_upper, text='Computer Dice: ', foreground='red')
+        player_dice_label.grid(row=4, column=0)
+        computer_dice_label.grid(row=4, column=2)
+
+        #Lower Frame
+        result_label = Label(frame_lower, text='Result', pady=15)
+        player_label = Label(frame_lower, text='Player: ', foreground='blue', pady=20)
+        computer_label = Label(frame_lower, text='Computer: ', foreground='red', pady=20)
+        result_label.grid(row=0, column=1)
+        player_label.grid(row=1, column=0, sticky=W)
+        computer_label.grid(row=1, column=2, sticky=E)
         master.minsize(width=300, height=300)
         master.mainloop()
-
 
 
 root = Tk()
